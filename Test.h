56efbd208c855a038cf850ea394750d584ba387f
kwebster248 <98540079+kwebster248@users.noreply.github.com>
2022-02-01 19:00:29 -0700

    Add files via upload
    Updated code

---
+++ Test.h
@@ -0,0 +1,13 @@
+// Defining each scenario
+
+#define PROMPT_MSG " You wake up from a deep sleep in an unfamiliar bedroom at midnight. \n There is a flashlight on the night stand next to the bed. \n You notice the flashlight has no batteries and there are none in the bedroom. \n You open the bedroom door and notice the hallway was bleak, cold and completley dark. \n As you step into the hallway you notice a bathroom to your left, but you hear \n screaming to your right down the stairs. \n What do you do next? \n a. Investigate noise towards the stairs \n b. Check out the bathroom \n"
+
+#define PROMPT_MSG1 " As you move closer to the stairs, the screaming is coming from a man and you notice \n a dim light coming from another room. What do you do next? \n a. Go back to the bathroom \n b. Go down the stairs towards the light"
+
+#define PROMPT_MSG2 " In the bathroom you realize the power is out after flipping the light switch, \n but you find batteries for the flashlight. With your flashlight you go towards the screaming. \n As you make your way down the stairs you notice the light is starting to fade. \n What do you do next? \n a. Go into room with fading light \n b. Wait on the stairs until the light fades to dark \n"
+
+#define PROMPT_MSG3 " As the light fades, the screaming turns into whimpering and crying. You hear him moving towards the stairs. \n What do you do next? a. Call out to the man b. Stay out of sight \n "
+
+#define PROMPT_MSG4 " When you walk into the room you see a man trembling while pointing \n a flashlight at a Manikan-like creature with no visible mouth. The creature has its arms raised at the man,\n but it is not moving. As his flashlight is dying he looks at you and says, \n  Keep your light on it! It cannot move when your looking at it! There is noise coming from the living room behind you. \n What do you do next? \n a. Investigate noise in living room \n b. Investigate Manakin-like creature \n c. Move towards the man"
+
+#define PROMPT_MSG5 " "
